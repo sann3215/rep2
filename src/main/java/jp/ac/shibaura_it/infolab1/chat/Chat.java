87055421d1da178c2c7efe1ae25ea40393fa0cdf
bp23064 <bp23064@shibaura-it.ac.jp>
2025-06-05 16:57:53 +0900

    演習５ 完了

--- src/main/java/jp/ac/shibaura_it/infolab1/chat/Chat.java
+++ src/main/java/jp/ac/shibaura_it/infolab1/chat/Chat.java
@@ -1,5 +1,7 @@
 package jp.ac.shibaura_it.infolab1.chat;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +14,21 @@ import java.util.List;
 public class Chat {
     private List<String>chats = new ArrayList(List.of("Kenji: Hello","Dai: Hello,too!"));
 
+    @Autowired
+    HttpSession session;
+
     @RequestMapping(value="/hello")
     String hello(Model model) {
         model.addAttribute("chats", chats);
+        model.addAttribute("name", session.getAttribute("name"));
         return "home.html";
     }
 
     @RequestMapping(value="/add")
     String addChat(@RequestParam("name") String name,
-                   @RequestParam("chat")String chat) {
+                   @RequestParam("chat")String chat, Model model) {
         chats.add(name + ":" +chat);
+        session.setAttribute("name", name);
         return "redirect:/hello";
     }
 }
